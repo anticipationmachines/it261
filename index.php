adfe179ff4b530d402b909b43760a6fad134d358
anticipationmachines <155695720+anticipationmachines@users.noreply.github.com>
2024-01-08 20:17:47 -0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Portal Page</title>
+    <link rel="stylesheet" href="css/styles.css" type="text/css">
+</head>
+<body>
+    <h1><a href="index.php">Fritz's Portal Page</a></h1>
+    <h2>The navigation below represnts our BIG Assignments</h2>
+
+<div id= "wrapper">
+        
+<nav>
+
+<ul>
+<li><a href="">Switch</a></li>
+<li><a href="">Troubleshoot</a></li>
+<li><a href="">Calculator</a></li>
+<li><a href="">Email</a></li>
+<li><a href="">Database</a></li>
+<li><a href="">Gallery</a></li>
+</ul>
+
+</nav>
+
+<main>
+<h2>About Fritz</h2>
+
+<img class="right" src="images/portrait.jpg" alt="portrait">
+
+<p>Phasellus nisi ante, euismod a enim cursus, interdum facilisis neque. Donec vulputate scelerisque turpis eget eleifend. Curabitur finibus tellus dolor, non aliquet purus mollis non. Proin dictum neque non sem pulvinar laoreet pretium at augue. Phasellus vel leo lorem. Ut nec neque massa. Aenean volutpat ipsum ac pretium condimentum. Sed tempus, neque et hendrerit tempor, nisi nisl sagittis sapien, id mattis tortor lectus nec purus. Suspendisse a dolor mattis, tempus dolor sed, luctus lacus. Phasellus risus ligula, tristique et luctus faucibus, hendrerit vitae dui. Proin vel massa ac purus suscipit feugiat dignissim in eros. Phasellus rutrum, dolor nec tempor pretium, diam urna pretium tortor, eu lacinia lacus tellus at nisl.</p>
+</main>
+
+<aside>
+<h2>Weekly Class Exercises</h2>
+<h3>Week 2</h3>
+<ul>
+<li><a href="var.php">var.php</a></li>
+<li><a href="">var2.php</a></li>
+</ul>
+
+</aside>
+
+</div>
+<!-- close wrapper -->
+</body>
+</html>
