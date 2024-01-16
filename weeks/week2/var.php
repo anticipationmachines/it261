8b9bdc1f427366141dda4b1cb98c24fbc5699165
anticipationmachines <155695720+anticipationmachines@users.noreply.github.com>
2024-01-16 02:42:44 -0800

    Add files via upload

---
+++ weeks/week2/var.php
@@ -0,0 +1,47 @@
+<?php
+// my variables
+
+$name = "Fritz"; //string
+echo $name;
+
+$body_temp = 98; //integer
+$body_temp_new = 98.6; //float
+echo '<br>';
+echo "<br>";
+echo $body_temp;
+echo "<br>";
+echo $body_temp_new;
+echo "<br>";
+echo "My name is $name?";
+echo "<br>";
+echo 'My name is '.$name.'?';
+echo "<br>";
+echo 'The normal body temperature for a human being is '.$body_temp_new.'';
+echo "<br>";
+$name = 'Pedro';
+echo $name;
+echo "<br>";
+$x = 20;
+$y = 5;
+
+$z = $x + $y; // "assigned term" vs "equals"
+
+echo $z;
+echo "<br>";
+$z = $x * $y;
+echo $z;
+
+echo "<br>";
+$z = $x / $y;
+echo $z;
+
+echo "<br>";
+$first_name = 'Fritz';
+$last_name = ' Codes';
+echo $first_name.' '.$last_name; 
+
+
+echo "<br>";
+echo "My full name is $first_name $last_name";
+echo "<br>";
+echo 'My full name is '.$first_name.' '.$last_name.'';
